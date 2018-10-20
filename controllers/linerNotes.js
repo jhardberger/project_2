1a5cfd63c96599101c21bb654da19e0712b0d1c5
John <john@Johns-MacBook-Pro.local>
2018-10-20 17:30:51 -0500

    set up restful routes for liner notes & albums

---
+++ controllers/linerNotes.js
@@ -0,0 +1,134 @@
+const express	 = require('express');
+const router 	 = express.Router();
+														//models
+const LinerNotes = require('../models/linerNotes');
+const Album   	 = require('../models/albums');
+														//routes
+//index
+router.get('/', async (req, res, next) => {
+
+	try {
+		const foundNotes = await LinerNotes.find();
+		res.render('linerNotes/index.ejs')
+	} catch(err){
+		next(err)
+	}
+		
+});
+//new
+router.get('/new', async (req, res, next) => {
+
+	try {
+		const allUsers = await User.find();
+		res.render('articles/new.ejs', {
+			users: allUsers
+		});
+										//need a way to find and pass thru current user
+	} catch(err){
+		next(err)
+	}
+
+});
+
+//show
+router.get('/:id', async (req, res, next) => {
+
+	try {
+		const foundNotes = await LinerNotes.findById(req.params.id);
+		const foundUser = await User.findOne({'linerNotes._id': req.params.id});
+		console.log(foundUser);
+		console.log(foundNotes);
+		res.render('linerNotes/show.ejs', {
+			linerNotes: foundUser, 
+			author: foundUser
+		});
+	} catch(err){
+		next(err)
+	}
+
+});
+
+//edit
+
+router.get('/:id/edit', async (req, res, next) => {
+
+	try {
+		const foundNote = await LinerNotes.findById(req.params.id);
+		const allUsers = await User.find();
+		const foundUser = await User.findOne({'linerNotes._id': req.params.id});
+		res.render('linerNotes/edit.ejs', {
+			linerNote: foundNotes,
+			users: allUsers,
+			user: foundUser
+		});	
+	} catch(err){
+		next(err)
+	}
+
+});
+
+//post
+
+router.post('/', async (req, res, next) => {
+	
+	try {
+		const foundUser = await User.findById(req.body.userId);
+		const createdNote = await LinerNotes.create(req.body);
+		foundUser.linerNotes.push(createdNote);
+		foundUser.save(() => {
+			res.redirect('/linerNotes');
+		})
+	} catch(err){
+		next(err)
+	}
+	
+});
+
+//put
+router.put('/:id', async (req, res, next) => {
+	
+	try {
+		const updatedNote = await LinerNotes.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		const foundUser = await User.findOne({'linerNotes._id': req.params.id});
+		if(foundUser._id.toString() !== req.body.userId){
+			foundUser.linerNotes.id(req.params.id).remove();
+
+			foundUser.save(async (req, res, next) => {
+				const newUser = await User.findById(req.body.userId);
+				newUser.linerNotes.push(updatedNote);
+				newuser.save(async (req, res, next) => {
+					res.redirect('/linerNotes')
+				});
+			});
+		}else{
+			foundUser.linerNotes.id(req.params.id).remove();
+			foundUser.linerNotes.push(updatedNote);
+			foundUser.save(() => {
+				res.redirect('/linerNotes')
+			});
+		}
+	} catch(err){
+		next(err)
+	}
+	
+});
+
+//delete
+
+router.delete('/:id', async (req, res, next) => {
+	
+	try {
+		const deletedNote = await LinerNotes.findByIdAndRemove(req.params.id);
+		const foundUser = User.findOne({'linerNotes._id': req.params.id});
+		foundUser.linerNotes.id(req.params.id).remove();
+		foundUser.Save(() => {
+			res.redirect('/articles')
+		});
+	} catch(err){
+		next(err)
+	}
+	
+});
+
+														//exports
+module.exports = router;
